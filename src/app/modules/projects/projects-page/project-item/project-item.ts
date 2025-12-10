41039628c40a0a9e0f0c68787767e8a6f285a491
elkingutierrex <elkin.gutierrex@gmail.com>
2025-12-10 09:19:17 -0500

    DELETE FILES

--- src/app/modules/projects/projects-page/project-item/project-item.ts
+++ src/app/modules/projects/projects-page/project-item/project-item.ts
@@ -10,8 +10,7 @@ import { Project } from '../../../../core/models/project.model';
 import { TaskStatus, Task } from '../../../../core/models/task.model';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateProjectTaskDialog } from '../../../../shared/components/create-project-task-dialog/create-project-task-dialog';
-
-
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-project-item',
@@ -22,10 +21,11 @@ import { CreateProjectTaskDialog } from '../../../../shared/components/create-pr
     MatButtonModule,
     MatExpansionModule,
     MatSelectModule,
-    MatIconModule
+    MatIconModule,
+    MatBadgeModule,
   ],
   templateUrl: './project-item.html',
-  styleUrls: ['./project-item.scss']
+  styleUrls: ['./project-item.scss'],
 })
 export class ProjectItem {
   dialog = inject(MatDialog);
@@ -44,34 +44,30 @@ export class ProjectItem {
   async changeStatus(task: Task, status: TaskStatus) {
     const updated = await this.tasksStore.updateStatus(task.id, status);
 
-    this.tasks.update(t =>
-      t.map(x => x.id === updated.id ? updated : x)
-    );
+    this.tasks.update((t) => t.map((x) => (x.id === updated.id ? updated : x)));
   }
 
-async createTask(type: 'Create Project' | 'Add Task') {
-
-  const dialogRef = this.dialog.open(CreateProjectTaskDialog, {
-  data: {
-      type,                   // ✔ Tipo de operación
-      projectId: this.project.id // ✔ ID del proyecto actual
-    }
-  });
-
-  dialogRef.afterClosed().subscribe(async result => {
-    if (!result) return;
-
-    console.log(result);
-
-
-    const created = await this.tasksStore.createTask(this.project.id!, {
-      title: result.name,
-      description: result.description,
-      status: result.status,
-      dueDate: result.startDate
+  async createTask(type: 'Create Project' | 'Add Task') {
+    const dialogRef = this.dialog.open(CreateProjectTaskDialog, {
+      data: {
+        type, // ✔ Tipo de operación
+        projectId: this.project.id, // ✔ ID del proyecto actual
+      },
     });
 
-    this.tasks.update(list => [created, ...list]);
-  });
-}
+    dialogRef.afterClosed().subscribe(async (result) => {
+      if (!result) return;
+
+      console.log(result);
+
+      const created = await this.tasksStore.createTask(this.project.id!, {
+        title: result.name,
+        description: result.description,
+        status: result.status,
+        dueDate: result.startDate,
+      });
+
+      this.tasks.update((list) => [created, ...list]);
+    });
+  }
 }
--- src/app/modules/projects/routes.ts
+++ src/app/modules/projects/routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 
 import { ProjectsPage } from './projects-page/projects-page';
 
-
 export const PROJECT_ROUTES: Routes = [
   {
     path: '',
@@ -10,18 +9,8 @@ export const PROJECT_ROUTES: Routes = [
     children: [
       {
         path: '',
-        loadComponent: () => import('./projects-page/projects-page').then((m) => m.ProjectsPage)
+        loadComponent: () => import('./projects-page/projects-page').then((m) => m.ProjectsPage),
       },
-      {
-        path: 'create',
-        loadComponent: () => import('./project-create/project-create').then((m) => m.ProjectCreate)
-      },
-      {
-        path: ':id',
-        loadComponent: () => import('./project-detail/project-detail').then((m) => m.ProjectDetail)
-      },
-
     ],
   },
-
 ];
